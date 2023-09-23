bbe04e4edf5c4b86fb4249840cb0a7df7b273c48
Robertscj <robertscj@tuta.io>
2023-09-23 10:00:49 -0300

    added basic feature: move a specific single file in the same directory

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+#os.path.expanduser() ver o caminho
+os.replace('/data/data/com.termux/files/home/storage/termux/file-move' + '/downloads.py', '/data/data/com.termux/files/home/storage/termux/file-move' + 'main.py')
